11202f5bf7be504884ac35bf0bbb3b1c6631306b
IvicVladica <ivicvladica@gmail.com>
2023-07-01 11:10:28 +0200

    Get method created

---
+++ src/main/java/com/example/MongoDB/StudentService.java
@@ -0,0 +1,18 @@
+package com.example.MongoDB;
+
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class StudentService {
+
+    private final StudentRepository studentRepository;
+
+    public List<Student> getAllStudents() {
+        return studentRepository.findAll();
+    }
+}
